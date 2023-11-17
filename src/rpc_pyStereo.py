41e34a298aafdb522dd73fdcea3eedc9dc7d7100
dohyeonYoon <xpdxpddld@jbnu.ac.kr>
2023-11-17 16:40:12 +0900

    feat: 이미지 캡처와 rpc를 비동기적으로 처리하는 기능 추가

--- src/rpc_pyStereo.py
+++ src/rpc_pyStereo.py
@@ -1,15 +1,16 @@
 '''
-Client Device
-(Capture images from both camera lens and send to calculate server by ftp protocol)
+Client Device working code
+(Capture images from both camera lens and send to cloud server by rpc method)
 '''
 
 import cv2
 import pyrpc
+import asyncio
 import os
-from datetime import datetime
+import datetime
 
 def capture_img():
-
+    ''' 양쪽 카메라 렌즈에서 이미지를 캡처하고 저장하는 코드'''
     # Open the left and right cameras.
     left_cam = cv2.VideoCapture(1)
     right_cam = cv2.VideoCapture(0)
@@ -23,24 +24,30 @@ def capture_img():
     # Capture images from both cameras.
     ret_left, left_frame = left_cam.read()
     ret_right, right_frame = right_cam.read()
-
-    # Save captured images to archive folder.
-    file_name = datetime.today().strftime("%Y_%m_%d_%H_%M_%S")
-    left_image_path = os.path.join('./archive/stereoL/', f'{file_name}_L.png')
-    right_image_path = os.path.join('./archive/stereoR/', f'{file_name}_R.png')
-    cv2.imwrite(left_image_path, left_frame)
-    cv2.imwrite(right_image_path, right_frame)
-
     return left_frame, right_frame
 
-def calculate_depth(left_frame, right_frame):
-    
-    # Depth estimation with both images in calculate server.
-    result = pyrpc.call("main", imgL=left_frame, imgR=right_frame)
+
+async def calculate_depth(left_frame, right_frame, serial_num):
+    '''클라우드 서버에서 스테레오 깊이추정 함수를 원격호출'''
+    # Depth estimation with both images in cloud server.
+    with pyrpc.Client('localhost', 5000) as client:
+        await client.call_async('main', serial_num, left_frame, right_frame)
+
+
+async def main():
+    # Set camera serial number.
+    serial_num = '0001'
+    while True:
+        current_time = datetime.datetime.now()
+        # Calculate next o'clock.
+        next_hour = (current_time + datetime.timedelta(hours=1)).replace(minute=0, second=0, microsecond=0)
+        # Wait until o'clock.
+        await asyncio.sleep((next_hour - current_time).total_seconds())
+        # Run every o'clock.
+        left_frame, right_frame = await capture_img()
+        await calculate_depth(left_frame, right_frame, serial_num)
 
 if __name__ == "__main__":
-    capture_img()
-    calculate_depth()
-    
+    asyncio.run(main())
 
 
